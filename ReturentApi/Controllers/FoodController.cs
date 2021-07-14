66ab879885de710e7e0bfa3190f6fc83146f2042
Ravinduprasath <prasathravindu2@gmail.com>
2021-07-14 20:25:42 +0530

    first

---
+++ ReturentApi/Controllers/FoodController.cs
@@ -0,0 +1,36 @@
+﻿using DataAcceessLibrary.BussinessLogic;
+using FoodOrder.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReturentApi.Controllers
+{
+    [ApiController]
+    [Route("api/food")]
+    public class FoodController : Controller
+    {
+        [HttpGet]
+        public List<FoodModel> Get()
+        {
+            var data = FoodClass.LoadFood();
+
+            List<FoodModel> food = new List<FoodModel>();
+
+            foreach (var row in data)
+            {
+                food.Add(new FoodModel
+                {
+                    Id = row.Id,
+                    FoodName = row.FoodName,
+                    FoodPrice = row.FoodPrice
+                });
+            }
+
+            return food;
+        }
+    }
+}
---
+++ ReturentApi/Controllers/OrderController.cs
@@ -0,0 +1,99 @@
+﻿using DataAcceessLibrary.BussinessLogic;
+using FoodOrder.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReturentApi.Controllers
+{
+    [ApiController]
+    [Route("api/order")]
+    public class OrderController : Controller
+    {   
+        [HttpPost]
+        public IActionResult Post(OrderModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var food = FoodClass.LoadFood();
+
+                model.Total = model.Quantity * food.Where(x => x.Id == model.FoodId).First().FoodPrice;
+
+                int id = OrderClass.InsertOrder(model.PersonName, model.PersonAddress, model.FoodId,
+                     model.Quantity, model.Total);
+
+                return Ok(new { Id = id });
+            }
+
+            return BadRequest(); 
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                var data = OrderClass.LoadOrder();
+
+                var val = data.Where(x => x.Id == id).FirstOrDefault();
+
+                OrderModel order = new OrderModel
+                {
+                    Id = val.Id,
+                    PersonName = val.PersonName,
+                    PersonAddress = val.PersonAddress,
+                    FoodId = val.FoodId,
+                    Quantity = val.Quantity,
+                    Total = val.Total
+                };
+                return Ok(order);
+            }
+            catch (Exception) 
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut]
+        public IActionResult Put(OrderModel model) 
+        {
+            try
+            {
+                var food = FoodClass.LoadFood();
+
+                model.Total = model.Quantity * food.Where(x => x.Id == model.FoodId).First().FoodPrice;
+
+                OrderClass.UpdateOrder(model.Id,
+                                       model.PersonName,
+                                       model.PersonAddress,
+                                       model.FoodId,
+                                       model.Quantity,
+                                       model.Total);
+
+                return Ok();
+            }
+            catch (Exception) 
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) 
+        {
+            try
+            {
+                OrderClass.DeleteOrder(id);
+                return Ok(id);
+            }
+            catch (Exception) 
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
